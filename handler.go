72d748be76b1aa89008919a512d1c68c502921a3
guochenye <guochenye@bytedance.com>
2022-11-27 02:10:15 +0800

    fix:修改

--- handler.go
+++ handler.go
@@ -1,1 +1,15 @@
-package user_core
+package main
+
+import (
+	"context"
+	"github.com/xiangqin/user_core/kitex_gen/user"
+)
+
+// UserImpl implements the last service interface defined in the IDL.
+type UserImpl struct{}
+
+// UserRegister implements the UserImpl interface.
+func (s *UserImpl) UserRegister(ctx context.Context, req *user.RegisterRequest) (resp *user.RegisterResponse, err error) {
+	// TODO: Your code here...
+	return
+}
--- main.go
+++ main.go
@@ -1,1 +1,14 @@
-package user_core
+package main
+
+import (
+"github.com/xiangqin/user_core/kitex_gen/user/user"
+	"log"
+)
+
+func main() {
+	svr := user.NewServer(new(UserImpl))
+	err := svr.Run()
+	if err != nil {
+		log.Println(err.Error())
+	}
+}
